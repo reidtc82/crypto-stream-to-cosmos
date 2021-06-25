728943e17feccac80bcabf33efd61ba77b990d43
reidtc82 <reidtc82@gmail.com>
2021-06-24 22:27:12 -0700

    linting

--- stream.py
+++ stream.py
@@ -11,10 +11,10 @@ from google.protobuf.json_format import MessageToJson
 import json
 import uuid
 
-HOST = config.settings['host']
-MASTER_KEY = config.settings['master_key']
-DATABASE_ID = config.settings['database_id']
-CONTAINER_ID = config.settings['container_id']
+HOST = config.settings["host"]
+MASTER_KEY = config.settings["master_key"]
+DATABASE_ID = config.settings["database_id"]
+CONTAINER_ID = config.settings["container_id"]
 
 # Set your API Key
 # cw.api_key = "123"
@@ -24,8 +24,8 @@ container = None
 # What to do on each trade update
 def handle_trades_update(trade_update):
     """
-        trade_update follows Cryptowatch protocol buffer format:
-        https://github.com/cryptowatch/proto/blob/master/public/markets/market.proto
+    trade_update follows Cryptowatch protocol buffer format:
+    https://github.com/cryptowatch/proto/blob/master/public/markets/market.proto
     """
     market_msg = ">>> Market#{} Exchange#{} Pair#{}: {} New Trades".format(
         trade_update.marketUpdate.market.marketId,
@@ -44,68 +44,81 @@ def handle_trades_update(trade_update):
             trade.amountStr,
         )
         print(trade_msg)
-        
+
         item = json.loads(MessageToJson(trade))
-        item['id'] = str(uuid.uuid4())
+        item["id"] = str(uuid.uuid4())
 
         global container
         container.create_item(body=item)
 
 
 def read_items(container):
-    print('\nReading all items in a container\n')
+    print("\nReading all items in a container\n")
 
     # NOTE: Use MaxItemCount on Options to control how many items come back per trip to the server
     #       Important to handle throttles whenever you are doing operations such as this that might
     #       result in a 429 (throttled request)
     item_list = list(container.read_all_items(max_item_count=10))
 
-    print('Found {0} items'.format(item_list.__len__()))
+    print("Found {0} items".format(item_list.__len__()))
 
     for doc in item_list:
-        print('Item Id: {0}'.format(doc.get('id')))
+        print("Item Id: {0}".format(doc.get("id")))
+
 
 def scale_container(container):
-    print('\nScaling Container\n')
-    
+    print("\nScaling Container\n")
+
     # You can scale the throughput (RU/s) of your container up and down to meet the needs of the workload. Learn more: https://aka.ms/cosmos-request-units
     try:
         offer = container.read_offer()
-        print('Found Offer and its throughput is \'{0}\''.format(offer.offer_throughput))
+        print("Found Offer and its throughput is '{0}'".format(offer.offer_throughput))
 
         offer.offer_throughput += 100
         container.replace_throughput(offer.offer_throughput)
 
-        print('Replaced Offer. Offer Throughput is now \'{0}\''.format(offer.offer_throughput))
-    
+        print(
+            "Replaced Offer. Offer Throughput is now '{0}'".format(
+                offer.offer_throughput
+            )
+        )
+
     except exceptions.CosmosHttpResponseError as e:
         if e.status_code == 400:
-            print('Cannot read container throuthput.')
+            print("Cannot read container throuthput.")
             print(e.http_error_message)
         else:
             raise
 
+
 def run_sample(subs, time_period):
-    client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY}, user_agent="CosmosDBPythonQuickstart", user_agent_overwrite=True)
+    client = cosmos_client.CosmosClient(
+        HOST,
+        {"masterKey": MASTER_KEY},
+        user_agent="CosmosDBPythonQuickstart",
+        user_agent_overwrite=True,
+    )
     try:
         # setup database for this sample
         try:
             db = client.create_database(id=DATABASE_ID)
-            print('Database with id \'{0}\' created'.format(DATABASE_ID))
+            print("Database with id '{0}' created".format(DATABASE_ID))
 
         except exceptions.CosmosResourceExistsError:
             db = client.get_database_client(DATABASE_ID)
-            print('Database with id \'{0}\' was found'.format(DATABASE_ID))
+            print("Database with id '{0}' was found".format(DATABASE_ID))
 
         # setup container for this sample
         global container
         try:
-            container = db.create_container(id=CONTAINER_ID, partition_key=PartitionKey(path='/partitionKey'))
-            print('Container with id \'{0}\' created'.format(CONTAINER_ID))
+            container = db.create_container(
+                id=CONTAINER_ID, partition_key=PartitionKey(path="/partitionKey")
+            )
+            print("Container with id '{0}' created".format(CONTAINER_ID))
 
         except exceptions.CosmosResourceExistsError:
             container = db.get_container_client(CONTAINER_ID)
-            print('Container with id \'{0}\' was found'.format(CONTAINER_ID))
+            print("Container with id '{0}' was found".format(CONTAINER_ID))
 
         scale_container(container)
 
@@ -118,9 +131,9 @@ def run_sample(subs, time_period):
 
         time.sleep(time_period)
         # create_items(container)
-        
+
         # read_item(container, 'SalesOrder1', 'Account1')
-        
+
         read_items(container)
         # query_items(container, 'Account1')
         # replace_item(container, 'SalesOrder1', 'Account1')
@@ -135,12 +148,12 @@ def run_sample(subs, time_period):
         #     pass
 
     except exceptions.CosmosHttpResponseError as e:
-        print('\nrun_sample has caught an error. {0}'.format(e.message))
+        print("\nrun_sample has caught an error. {0}".format(e.message))
     finally:
-            print("\nrun_sample done")
-            # Call disconnect to close the stream connection
-            cw.stream.disconnect()
+        print("\nrun_sample done")
+        # Call disconnect to close the stream connection
+        cw.stream.disconnect()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_sample(["markets:*:trades"], 1)
